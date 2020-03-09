1b32b3b0f9d2c40e30957eaec938c84f5a5af90b
Fedor Sergeev <sergeev.fi@phystech.edu>
2020-03-09 23:00:58 +0300

    Positive energies generator, discriminator fix, full dataset traning

--- analysis/discriminator.py
+++ analysis/discriminator.py
@@ -4,32 +4,26 @@ import torch.nn.functional as F
 
 from torch.nn.utils import spectral_norm
 
+
 class ModelD(nn.Module):
     def __init__(self):
         super(ModelD, self).__init__()
         
         # 30x30x1
         self.conv1 = spectral_norm(nn.Conv2d( 1,   32, kernel_size=(3,3), stride=(1,1), padding=(1,1))) # 30x30x32
-        #self.ln1   = nn.LayerNorm([32, 30, 30])
         self.conv2 = spectral_norm(nn.Conv2d( 32,  64, kernel_size=(3,3), stride=(1,1), padding=(1,1))) # 30x30x64
-        #self.ln2   = nn.LayerNorm([64, 30, 30])        
-        self.pool1 = nn.MaxPool2d(2, 2)                                                  # 15x15x64
+        self.pool1 = nn.MaxPool2d(2, 2)                                                                 # 15x15x64
 
         self.conv3 = spectral_norm(nn.Conv2d( 64, 128, kernel_size=(3,3), stride=(1,1), padding=(1,1))) # 15x15x128
-        #self.ln3   = nn.LayerNorm([128, 15, 15])
         self.conv4 = spectral_norm(nn.Conv2d(128, 256, kernel_size=(3,3), stride=(1,1), padding=(1,1))) # 15x15x256
-        #self.ln4   = nn.LayerNorm([256, 15, 15])
-        self.pool2 = nn.MaxPool2d(3, 3)                                                  # 15x15x256
+        self.pool2 = nn.MaxPool2d(3, 3)                                                                 # 15x15x256
         
         # 5x5x256 = 6400
-        self.fc1 = spectral_norm(nn.Linear(6400 + 5, 1))
+        self.fc1 = spectral_norm(nn.Linear(6400 + 5, 128))
+        self.fc2 = spectral_norm(nn.Linear(128,32))
+        self.fc3 = spectral_norm(nn.Linear(32,1))
         
     def forward(self, EnergyDeposit, ParticleMomentum_ParticlePoint):
-        MEAN_TRAIN_MOM_POINT = torch.Tensor([-0.08164814, -0.02489864, 20.8446184, 
-                                             -0.01204223,  0.02772552])
-        STD_TRAIN_MOM_POINT  = torch.Tensor([ 5.4557047,   5.38253167, 24.26102735, 
-                                              2.69435522,  2.65776869])
-
         X = EnergyDeposit
         X = F.leaky_relu(self.conv1(X))
         X = F.leaky_relu(self.conv2(X))
@@ -40,13 +34,19 @@ class ModelD(nn.Module):
         X = self.pool2(X)
         
         X = X.view(len(X), -1)
+
+        # for both electrons and photons
+        MEAN_TRAIN_MOM_POINT = torch.Tensor([-0.02729166, -0.02461355, 20.90919671, 
+                                             -0.00788923,  0.00720004])
+        STD_TRAIN_MOM_POINT  = torch.Tensor([ 5.48167024,  5.43016916, 24.32682144,  
+                                              2.69976438,  2.67467291])
         mom_point = (ParticleMomentum_ParticlePoint - MEAN_TRAIN_MOM_POINT) / STD_TRAIN_MOM_POINT
         X = torch.cat([X, mom_point], dim=1)
         
-        #X = F.leaky_relu(self.fcbn1(self.fc1(X)))
-        #X = F.leaky_relu(self.fcbn2(self.fc2(X)))
+        X = F.leaky_relu(self.fc1(X))
+        X = F.leaky_relu(self.fc2(X))
         #X = F.leaky_relu(self.fcbn3(self.fc3(X)))
         if TASK in ['WASSERSTEIN', 'HINGE']:
-            return self.fc1(X)
+            return self.fc3(X)
         else:
-            return torch.sigmoid(self.fc1(X))
+            return torch.sigmoid(self.fc3(X))
--- analysis/generator.py
+++ analysis/generator.py
@@ -5,6 +5,7 @@ from torch.nn.utils import spectral_norm
 
 NOISE_DIM = 10
 
+# PositiveEnergyReLUGenerator
 class ModelGConvTranspose(nn.Module):
     def __init__(self, z_dim):
         self.z_dim = z_dim
@@ -15,31 +16,23 @@ class ModelGConvTranspose(nn.Module):
         self.fc4 = nn.Linear(512, 20736)
 
         self.conv1 = spectral_norm(nn.ConvTranspose2d(256, 256, 3, stride=2, output_padding=1))
-        #self.bn1 = ConditionalBatchNorm2d(5, 256)
         self.bn1 = nn.BatchNorm2d(256)
-        #self.ln1 = nn.LayerNorm([256, 20, 20])
         self.conv2 = spectral_norm(nn.ConvTranspose2d(256, 128, 3))
-        #self.bn2 = ConditionalBatchNorm2d(5, 128)
         self.bn2 = nn.BatchNorm2d(128)
-        #self.ln2 = nn.LayerNorm([128, 22, 22])
         self.conv3 = spectral_norm(nn.ConvTranspose2d(128, 64, 3))
-        #self.bn3 = ConditionalBatchNorm2d(5, 64)
         self.bn3 = nn.BatchNorm2d(64)
-        #self.ln3 = nn.LayerNorm([64, 24, 24])
         self.conv4 = spectral_norm(nn.ConvTranspose2d(64, 32, 3))
-        #self.bn4 = ConditionalBatchNorm2d(5, 32)
         self.bn4 = nn.BatchNorm2d(32)
-        #self.ln4 = nn.LayerNorm([32, 26, 26])
         self.conv5 = spectral_norm(nn.ConvTranspose2d(32, 16, 3))
         self.bn5 = nn.BatchNorm2d(16)
-        #self.ln5 = nn.LayerNorm([16, 28, 28])
         self.conv6 = spectral_norm(nn.ConvTranspose2d(16, 1, 3))
         
     def forward(self, z, ParticleMomentum_ParticlePoint):
-        MEAN_TRAIN_MOM_POINT = torch.Tensor([-0.08164814, -0.02489864, 20.8446184, 
-                                             -0.01204223,  0.02772552])
-        STD_TRAIN_MOM_POINT  = torch.Tensor([ 5.4557047,   5.38253167, 24.26102735, 
-                                              2.69435522,  2.65776869])
+        # for both electrons and photons
+        MEAN_TRAIN_MOM_POINT = torch.Tensor([-0.02729166, -0.02461355, 20.90919671, 
+                                             -0.00788923,  0.00720004])
+        STD_TRAIN_MOM_POINT  = torch.Tensor([ 5.48167024,  5.43016916, 24.32682144,  
+                                              2.69976438,  2.67467291])
 
         mom_point = (ParticleMomentum_ParticlePoint - MEAN_TRAIN_MOM_POINT) / STD_TRAIN_MOM_POINT
         #mom_point = ParticleMomentum_ParticlePoint
@@ -50,19 +43,14 @@ class ModelGConvTranspose(nn.Module):
         x = F.leaky_relu(self.fc4(x))
         EnergyDeposit = x.view(-1, 256, 9, 9)
         
-        #print(EnergyDeposit.shape)
-        #EnergyDeposit = F.leaky_relu(self.bn2(self.conv2(EnergyDeposit), mom_point))
-        #EnergyDeposit = F.leaky_relu(self.bn3(self.conv3(EnergyDeposit), mom_point))
-        #EnergyDeposit = F.leaky_relu(self.bn4(self.conv4(EnergyDeposit), mom_point))
         EnergyDeposit = F.leaky_relu(self.bn1(self.conv1(EnergyDeposit)))
         EnergyDeposit = F.leaky_relu(self.bn2(self.conv2(EnergyDeposit)))
         EnergyDeposit = F.leaky_relu(self.bn3(self.conv3(EnergyDeposit)))
         EnergyDeposit = F.leaky_relu(self.bn4(self.conv4(EnergyDeposit)))
         EnergyDeposit = F.leaky_relu(self.bn5(self.conv5(EnergyDeposit)))
-        EnergyDeposit = self.conv6(EnergyDeposit)
+        EnergyDeposit = F.relu(self.conv6(EnergyDeposit))
 
         return EnergyDeposit
-
 
 class EnsembleGenerator(nn.Module):
     def __init__(self, modelA, modelB):
